8d5ddf3c66d6fb7f189976a660eedbc4ccb748da
M.RWEMA REMY <rwemaremy21@gmail.com>
2024-04-04 15:29:01 +0200

    done with humberger-btn

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,5 @@
 let image = document.getElementById("image");
+
 let pictures = [
   "/IMAGES/jordan3.png",
   "/IMAGES/jordan4.png",
@@ -10,3 +11,8 @@ setInterval(function () {
   let rondom = Math.floor(Math.random() * 5);
   image.src = pictures[rondom];
 }, 2700);
+
+function toggleMenu() {
+  const menu = document.querySelector(".nav-bar-content");
+  menu.classList.toggle("show");
+}
